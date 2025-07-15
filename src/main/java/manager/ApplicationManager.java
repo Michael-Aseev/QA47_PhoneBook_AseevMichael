5fb17beea5602f36d6dc382828e0f66fbdf9c6a5
michael <michaelaseev27@gmail.com>
2025-07-15 20:43:08 +0300

    new

--- src/main/java/manager/ApplicationManager.java
+++ src/main/java/manager/ApplicationManager.java
@@ -1,7 +1,11 @@
 package manager;
 
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.Browser;
 import org.openqa.selenium.support.events.EventFiringDecorator;
 import org.openqa.selenium.support.events.WebDriverListener;
 import org.slf4j.Logger;
@@ -11,6 +15,7 @@ import org.testng.annotations.BeforeMethod;
 import utils.WDListener;
 
 import java.time.Duration;
+import java.time.LocalDate;
 
 public class ApplicationManager {
     private WebDriver driver;
@@ -21,10 +26,36 @@ public class ApplicationManager {
 
     public Logger logger = LoggerFactory.getLogger(ApplicationManager.class);
 
+    //==========================
+    static String browser = System.getProperty("browser", "chrome");
+
+    public ApplicationManager(){
+
+    }
+
+
     @BeforeMethod(alwaysRun = true)
     public void setup() {
 //      logger.info("Start test --> " + LocalDate.now());
-        driver = new ChromeDriver();
+       // driver = new ChromeDriver();
+        switch (browser.toLowerCase()){
+            case "firefox":
+                driver = new FirefoxDriver();
+                logger.info("Start test in browser Firefox");
+                break;
+            case "edge":
+                driver = new EdgeDriver();
+                logger.info("Start test in browser Edge");
+                break;
+            case "chrome":
+                driver = new ChromeDriver();
+                logger.info("Start test in browser Chrome");
+                break;
+            default:
+                driver = new ChromeDriver();
+                logger.info("Start test in browser Chrome");
+                break;
+        }
         driver.manage().window().maximize();
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
         WebDriverListener webDriverListener = new WDListener();
